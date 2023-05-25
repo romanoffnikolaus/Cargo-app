4a12182efe7a61ae29bce5608ffbcac1d5ee9963
romanoffnikolaus <romanoffnikolaus@gmail.com>
2023-05-25 20:30:54 +0600

    volume

--- application/apps.py
+++ application/apps.py
@@ -5,9 +5,9 @@ class ApplicationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'application'
 
-    # def ready(self):
-    #     from .add_utils.csv import import_csv_to_database
-    #     try:
-    #         import_csv_to_database('uszips.csv')
-    #     except:
-    #         ...
+    def ready(self):
+        from .add_utils.csv import import_csv_to_database
+        try:
+            import_csv_to_database('uszips.csv')
+        except:
+            ...
